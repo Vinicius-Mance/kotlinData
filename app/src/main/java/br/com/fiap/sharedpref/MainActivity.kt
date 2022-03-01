a2dd601c1dae2e8f2adf1180ac5568ea4c9d85a9
Vinicius-Mance <vinimanceprof@gmail.com>
2022-02-28 22:35:26 -0300

    modo via arquivo txt adicionado

--- app/src/main/java/br/com/fiap/sharedpref/MainActivity.kt
+++ app/src/main/java/br/com/fiap/sharedpref/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +23,41 @@ class MainActivity : AppCompatActivity() {
         val listTratamento = findViewById<Spinner>(R.id.listTratamento)
 
         btnSalvar.setOnClickListener{
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+          /*  val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
             val editor = saudacaoPersistencia.edit()
 
             editor.putString("nome",txtNome.editableText.toString())
             editor.putString("tratamento", listTratamento.selectedItem.toString())
             editor.apply()
+
+
+            */
             Toast.makeText(this, "Saudação gravada", Toast.LENGTH_SHORT).show()
+
+            val dados = txtNome.editableText.toString() + ":" + listTratamento.selectedItem.toString()
+            gravarDadoArquivo("saudacao", dados)
+
         }
 
         btnExibir.setOnClickListener{
             val link = Intent(this,Saudacao::class.java)
             startActivity(link)
         }
+    }
+
+    fun gravarDadoArquivo(fileName : String, data : String) {
+
+        try {
+            val arquivo = openFileOutput(fileName,Context.MODE_PRIVATE)
+
+            arquivo.write(data.toByteArray())
+            arquivo.close()
+
+        } catch (e: FileNotFoundException) {
+            Log.i("gravaDadoArquivo", "Fudeu baiah, roubaram o arquivo menó!")
+        } catch (e: IOException) {
+            Log.i("gravaDadoArquivo", "mano... o bagulho tá sinistro kkkkk EXORCISA!")
+        }
+
     }
 }
--- app/src/main/java/br/com/fiap/sharedpref/Saudacao.kt
+++ app/src/main/java/br/com/fiap/sharedpref/Saudacao.kt
@@ -3,7 +3,13 @@ package br.com.fiap.sharedpref
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
+import kotlin.text.Charsets.UTF_8
 
 class Saudacao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +18,7 @@ class Saudacao : AppCompatActivity() {
 
         val txvSaudacao = findViewById<TextView>(R.id.txvSaudacao)
 
-        val saudacaoSharedPref = this.getSharedPreferences("saudacao",Context.MODE_PRIVATE)
+       /* val saudacaoSharedPref = this.getSharedPreferences("saudacao",Context.MODE_PRIVATE)
 
         val nome = saudacaoSharedPref.getString("nome", "Usuário(a)")
         val tratamento = saudacaoSharedPref.getString("tratamento", "Sr(a)")
@@ -21,6 +27,37 @@ class Saudacao : AppCompatActivity() {
             txvSaudacao.text = nome;
         } else {
             txvSaudacao.text = tratamento + " " + nome
+        } */
+
+        val data = recuperaDadoArquivo("saudacao")
+        Log.i("recuperaDadoArquivo", "dados: "+data)
+        val tokenizer = StringTokenizer(data, ":")
+        val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "sem nome"
+        val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "sem tratamento"
+
+        if (tratamento.equals("N/A")) {
+            txvSaudacao.text = nome
+        } else {
+            txvSaudacao.text = tratamento + " " + nome
         }
     }
+
+   fun recuperaDadoArquivo(fileName : String) : String {
+
+       try {
+           val arquivo = openFileInput(fileName)
+           val dados = arquivo.readBytes()
+
+           arquivo.close()
+
+           return dados.toString(Charset.defaultCharset())
+
+       } catch (e: FileNotFoundException) {
+           Log.i("recuperaDadoArquivo", "Fudeu baiah, roubaram o arquivo menó!")
+           return ""
+       } catch (e: IOException) {
+           Log.i("recuperaDadoArquivo", "mano... o bagulho tá sinistro kkkkk EXORCISA!")
+           return ""
+       }
+   }
 }
